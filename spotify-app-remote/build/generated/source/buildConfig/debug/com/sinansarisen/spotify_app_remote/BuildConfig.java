0ae03d2c4bba567412bc59acc347d6cb8cd2283d
sinan.sarisen <sinan.sarisen@turuncuholding.com.tr>
2023-09-29 11:07:21 +0300

    spoty

---
+++ spotify-app-remote/build/generated/source/buildConfig/debug/com/sinansarisen/spotify_app_remote/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sinansarisen.spotify_app_remote;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.sinansarisen.spotify_app_remote";
+  public static final String BUILD_TYPE = "debug";
+}
